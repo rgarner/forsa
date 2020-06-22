0aabebbaedf4ed7c1876b8818097f9e2030fef69
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-06-22 14:24:40 +0100

    Add AUTOADDRESS_KEY to environment.js
    If we don't do this, the API key will be missing from the development
    environment. It's picked up from .env (corresponding to heroku
    config:set) automatically during the production deploy, but locally
    we require an EnvironmentPlugin per
    
    https://github.com/rails/webpacker/blob/master/docs/env.md
    and
    https://github.com/rails/webpacker/issues/1641#issuecomment-411505653

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,19 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
+const dotenv = require('dotenv')
 
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery'
   })
+)
+
+dotenv.config({ path: '.env', silent: true })
+environment.plugins.insert(
+  "Environment",
+  new webpack.EnvironmentPlugin(process.env)
 )
 
 module.exports = environment
